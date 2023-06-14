b107606b372e9a683586d84e871cfb6f19ce540c
fuwamaki <variety.fuwa.3@gmail.com>
2023-06-14 16:08:45 +0900

    Initial Commit

---
+++ SampleSwiftUI3/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SampleSwiftUI3
+//
+//  Created by yusaku maki on 2023/06/14.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SampleSwiftUI3/SampleSwiftUI3App.swift
@@ -0,0 +1,20 @@
+//
+//  SampleSwiftUI3App.swift
+//  SampleSwiftUI3
+//
+//  Created by yusaku maki on 2023/06/14.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SampleSwiftUI3App: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: Item.self)
+    }
+}
