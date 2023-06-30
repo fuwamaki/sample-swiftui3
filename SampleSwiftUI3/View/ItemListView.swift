01cbd27ced38aecc9710d588fd663e210278fc15
fuwamaki <variety.fuwa.3@gmail.com>
2023-06-30 10:25:55 +0900

    create ItemListView

---
+++ SampleSwiftUI3/View/ItemListView.swift
@@ -0,0 +1,59 @@
+//
+//  ItemListView.swift
+//  SampleSwiftUI3
+//
+//  Created by yusaku maki on 2023/06/30.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ItemListView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+
+    var body: some View {
+        List {
+            ForEach(items) { item in
+                NavigationLink {
+                    Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                } label: {
+                    Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                }
+            }
+            .onDelete(perform: deleteItems)
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                EditButton()
+            }
+            ToolbarItem {
+                Button(action: addItem) {
+                    Label("Add Item", systemImage: "plus")
+                }
+            }
+        }
+        .navigationTitle("Select an item")
+    }
+
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(timestamp: Date())
+            modelContext.insert(newItem)
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
+    }
+}
+
+#Preview {
+    ItemListView()
+        .modelContainer(for: Item.self, inMemory: true)
+        .environment(\.locale, .init(identifier: "ja"))
+}
